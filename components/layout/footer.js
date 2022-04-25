fb446f40b793070d29d1ba7b6fc83163c60fac1e
Herman van Dompseler <herman@dompseler.nl>
2022-04-25 22:19:07 +0200

    linkje naar contact

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -59,7 +59,7 @@ function Footer(props) {
                 <Col>
                     <b>Meer</b>
                     <ul>
-                        <li><a href="/">Contact</a></li>
+                        <li><a href="/contact">Contact</a></li>
                         <li><a href="/">Terug naar het begin</a></li>
                     </ul>
                     <p>Being a yogi is een initiatief van het <a href="https://bamboehuis.amsterdam">Bamboehuis</a>. &copy; 2022</p>
