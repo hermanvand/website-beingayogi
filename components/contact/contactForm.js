eea50129f078b4900b17ce1c10263be9e1ab9bba
Herman van Dompseler <herman@dompseler.nl>
2022-04-27 22:53:15 +0200

    voeg name attribuut toe

--- components/contact/contactForm.js
+++ components/contact/contactForm.js
@@ -36,21 +36,21 @@ function ContactForm () {
                 <Col>        
                     <Form id="myForm" action="https://getform.io/f/a782df2c-bc0a-4279-a29d-6386d02c9ff7" method="post" noValidate validated={validated} onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="formulier">
-                            <Form.Control type="hidden" value="beingayogi-contact" />
+                            <Form.Control type="hidden" name="formulier" value="beingayogi-contact" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="naam">
                             <Form.Label>Hoe heet je?</Form.Label>
-                            <Form.Control required type="text" placeholder="Naam" />
+                            <Form.Control required type="text" name="naam" placeholder="Naam" />
                             <Form.Control.Feedback type="invalid">Wat is je naam?</Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="email">
                             <Form.Label>Wat is je emailadres?</Form.Label>
-                            <Form.Control required type="email" placeholder="naam@voorbeeld.nl" />
+                            <Form.Control required type="email" name="email" placeholder="naam@voorbeeld.nl" />
                             <Form.Control.Feedback type="invalid">Gebruik een geldig emailadres.</Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="opmerking">
                             <Form.Label>Waar kunnen we je mee helpen?</Form.Label>
-                            <Form.Control required as="textarea" rows={4} placeholder="Stel hier je vraag"/>
+                            <Form.Control required as="textarea" name="opmerking" rows={4} placeholder="Stel hier je vraag"/>
                             <Form.Control.Feedback type="invalid">Wat is je vraag?</Form.Control.Feedback>
                         </Form.Group>
                         <Button type="submit">Verstuur</Button>
